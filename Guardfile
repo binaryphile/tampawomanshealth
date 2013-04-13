995af867f24dbf14d5409dcf1d94b4d688f4ca7c
Ted Lilley <tlilley@diditbetter.com>
2013-04-13 18:19:29 +0000

    Initial theme import, Guardfile

---
+++ Guardfile
@@ -0,0 +1,11 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'livereload', grace_period: 1 do
+  watch(%r{pages})
+  watch(%r{posts})
+  watch(%r{partials})
+  watch(%r{themes})
+  watch(%r{data.yml})
+end
+
---
+++ themes/artisteer/javascripts/button.js
@@ -0,0 +1,22 @@
+/* begin Page */
+/* Created by Artisteer v3.1.0.46558 */
+// css helper
+/* begin Button */
+function artButtonSetup(className) {
+    jQuery.each(jQuery("a." + className + ", button." + className + ", input." + className), function (i, val) {
+        var b = jQuery(val);
+        if (!b.parent().hasClass('art-button-wrapper')) {
+            if (b.is('input')) b.val(b.val().replace(/^\s*/, '')).css('zoom', '1');
+            if (!b.hasClass('art-button')) b.addClass('art-button');
+            jQuery("<span class='art-button-wrapper'><span class='art-button-l'> </span><span class='art-button-r'> </span></span>").insertBefore(b).append(b);
+            if (b.hasClass('active')) b.parent().addClass('active');
+        }
+        b.mouseover(function () { jQuery(this).parent().addClass("hover"); });
+        b.mouseout(function () { var b = jQuery(this); b.parent().removeClass("hover"); if (!b.hasClass('active')) b.parent().removeClass('active'); });
+        b.mousedown(function () { var b = jQuery(this); b.parent().removeClass("hover"); if (!b.hasClass('active')) b.parent().addClass('active'); });
+        b.mouseup(function () { var b = jQuery(this); if (!b.hasClass('active')) b.parent().removeClass('active'); });
+    });
+}
+jQuery(function() { artButtonSetup("art-button-old"); });
+
+/* end Button */
